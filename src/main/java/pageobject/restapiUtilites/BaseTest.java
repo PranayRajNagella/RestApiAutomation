2dddca9a1d754fb6537538e8ae28cf2aebc227be
91830 <ravipranay27@gmail.com>
2022-03-01 22:07:53 +0530

    Extent Factory Update

--- src/main/java/pageobject/restapiUtilites/BaseTest.java
+++ src/main/java/pageobject/restapiUtilites/BaseTest.java
@@ -2,26 +2,32 @@ package pageobject.restapiUtilites;
 
 import java.io.IOException;
 import org.apache.poi.ss.usermodel.Sheet;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentReporter;
 
 import pageobject.DataAndPropertyUtils.ExcelDataUtil;
 import pageobject.DataAndPropertyUtils.ExtentReportsUtils;
 import pageobject.DataAndPropertyUtils.JsonDataUtil;
 import pageobject.DataAndPropertyUtils.PropertyReadWriteUtils;
+import pageobject.factory.Factory;
 
 public class BaseTest extends RestBase{
 
-	protected  ExcelDataUtil EDU;
-	protected  Sheet sheet;
-	protected JsonDataUtil JSD;
-	protected PropertyReadWriteUtils Property;
-	protected ExtentTest test;
-	protected ExtentReports report;
-	protected ExtentReportsUtils extentReport;
+	protected static ExcelDataUtil EDU;
+	protected static Sheet sheet;
+	protected static JsonDataUtil JSD;
+	protected static PropertyReadWriteUtils Property;
+	protected static ExtentReports report;
+	protected static RestValidations restValidater;
+	protected static ExtentTest test;
 	
 	@BeforeSuite(groups={"ApiTest","Regression","Sanity"})
 	public void setUpUtils() throws Exception
@@ -29,8 +35,8 @@ public class BaseTest extends RestBase{
 		
 		EDU=new ExcelDataUtil();
 		JSD=new JsonDataUtil();
-		extentReport=new ExtentReportsUtils();
-		report=extentReport.setUpExtent();
+		report=ExtentReportsUtils.setUpExtent();
+		restValidater=new RestValidations();
 		sheet=EDU.getSheetFromExcel(System.getProperty("user.dir")+"\\src\\test\\Resources\\TestData (3).xlsx","TestData");
 		Property=new PropertyReadWriteUtils(System.getProperty("user.dir")+"\\src\\test\\Resources\\constant.properties");
 	}
--- src/main/java/pageobject/restapiUtilites/RestValidations.java
+++ src/main/java/pageobject/restapiUtilites/RestValidations.java
@@ -1,13 +1,26 @@
 package pageobject.restapiUtilites;
 
 import org.testng.Assert;
-
+import com.aventstack.extentreports.ExtentTest;
 import io.restassured.response.Response;
+import pageobject.factory.Factory;
 
-public class RestValidations {
+public class RestValidations{
 
-	public static void StatusCodeValidation(Response response,int statusCodeExpected)
+	public void StatusCodeValidation(Response response,int statusCodeExpected, ExtentTest extentTest)
 	{
+		System.err.println("inside the method conition");
+		if(response.getStatusCode()==statusCodeExpected)
+		{
+			System.err.println("inside if tru conition");
+			//extentTest.pass("Actuall Status Code is "+response.getStatusCode()+" Expected Statuc Code "+statusCodeExpected);
+			Factory.getFactory().getReporter().pass("Actuall Status Code is "+response.getStatusCode()+" Expected Statuc Code "+statusCodeExpected);
+		}
+		else
+		{
+			//extentTest.fail("Actuall Status Code is "+response.getStatusCode()+" Expected Statuc Code "+statusCodeExpected);	
+			Factory.getFactory().getReporter().fail("Actuall Status Code is "+response.getStatusCode()+" Expected Statuc Code "+statusCodeExpected);
+		}
 		Assert.assertEquals(response.getStatusCode(), statusCodeExpected,"Expected status code is"+statusCodeExpected+" Actuall status code returned"+response.getStatusCode());
 	}
 	
